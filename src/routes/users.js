73a0baf5db1d805f39558d8b99ea077a3f1b0c17
Abdulkbk <abdoollahikbk@gmail.com>
2022-12-24 21:17:48 +0100

    restructure

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,28 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const createError = require('http-errors')
+const { getUsersController, addUserController } = require('../controllers/user')
 
 const users = [{ id: 1, name: 'Abd Yun', email: 'aa@aa.com' }]
 
-router
-  .get('/', (req, res) => {
-    res.json(users)
-  })
-  .post('/', (req, res, next) => {
-    const { name, email } = req.body
-
-    if ((typeof name !== 'string') | (typeof email !== 'string')) {
-      return next(createError(422, 'Name and Email must be string'))
-    }
-
-    const id = users.length + 1
-
-    const newUser = { id, name, email }
-
-    users.push(newUser)
-
-    res.status(201).json(newUser)
-  })
+router.get('/', getUsersController).post('/', addUserController)
 
 router.get('/:id', (req, res, next) => {
   const foundUser = users.find(user => user.id === Number(req.params.id))
